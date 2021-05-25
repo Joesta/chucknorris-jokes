46bde3e6c86caa076b64716f6dc99423dcbf232b
Joesta <joestax@icloud.com>
2021-05-25 17:39:17 +0200

    Added glide, recyclerview library and implemented a basic layout and used recycler view for showing  chuck jokes

--- app/src/main/java/io/chucknorris/assessment/activities/MainActivity.java
+++ app/src/main/java/io/chucknorris/assessment/activities/MainActivity.java
@@ -1,14 +1,16 @@
 package io.chucknorris.assessment.activities;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import io.chucknorris.assessment.R;
+import io.chucknorris.assessment.adapter.JokeAdapter;
 import io.chucknorris.assessment.models.Joke;
 import io.chucknorris.assessment.retrofit.RetrofitClient;
 import io.chucknorris.assessment.service.JokeService;
@@ -18,30 +20,48 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        JokeService service =
-                RetrofitClient
-                        .getInstance()
-                        .create(JokeService.class);
+        initUI();
+        getJokes();
+    }
 
+    private void getJokes() {
+        RetrofitClient.getInstance()
+                .create(JokeService.class)
+                .getRandomJoke()
+                .enqueue(new Callback<Joke>() {
+                    @Override
+                    public void onResponse(Call<Joke> call, Response<Joke> response) {
+                        if (response.isSuccessful()) {
+                            Joke joke = response.body();
+                            List<Joke> jokes = new ArrayList<>();
+                            jokes.add(joke);
+                            mAdapter = new JokeAdapter(MainActivity.this, jokes);
+                            recyclerView.setAdapter(mAdapter);
+                        }
+                    }
 
-        service.getRandomJoke().enqueue(new Callback<Joke>() {
-            @Override
-            public void onResponse(Call<Joke> call, Response<Joke> response) {
-                if (response.isSuccessful()) {
-                    Joke body = response.body();
-                    Log.d("joke", body.toString());
-                }
-            }
+                    @Override
+                    public void onFailure(Call<Joke> call, Throwable t) {
+                        t.printStackTrace();
+                    }
+                });
+    }
 
-            @Override
-            public void onFailure(Call<Joke> call, Throwable t) {
-                t.printStackTrace();
-            }
-        });
+    private void initUI() {
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        // initializing layout manager for recycler view
+        layoutManager = new LinearLayoutManager(MainActivity.this, RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
---
+++ app/src/main/java/io/chucknorris/assessment/adapter/JokeAdapter.java
@@ -0,0 +1,96 @@
+package io.chucknorris.assessment.adapter;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import io.chucknorris.assessment.R;
+import io.chucknorris.assessment.models.Joke;
+
+/**
+ * Project Name - chucknorris-jokes
+ * Created on 2021/05/25 at 4:47 PM
+ */
+public class JokeAdapter extends RecyclerView.Adapter<JokeAdapter.ViewHolder> {
+
+
+    private Context context;
+    private List<Joke> jokes;
+
+
+    public JokeAdapter(Context context, List<Joke> jokes) {
+        this.context = context;
+        this.jokes = jokes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(
+                LayoutInflater.from(context).inflate(R.layout.item, parent, false));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Joke joke = jokes.get(position);
+
+        Glide.with(context)
+                .load(joke.getIconURL())
+                .into(holder.chuckImage);
+
+
+        // Todo - come back and fix me
+        if (!joke.getCategories().isEmpty()) {
+            joke.getCategories().forEach(
+                    categoryTitle -> Log.d("category", categoryTitle)
+            );
+        }
+
+        holder.text_createdAt.setText(joke.getCreatedAt().toString());
+        holder.text_updatedAt.setText(joke.getUpdatedAt().toString());
+        holder.text_joke.setText(joke.getValue());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.jokes.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView chuckImage;
+        TextView text_createdAt;
+        TextView text_updatedAt;
+        TextView text_category;
+        TextView text_joke;
+
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            initUI();
+        }
+
+        private void initUI() {
+            chuckImage = itemView.findViewById(R.id.chuck_image);
+            text_createdAt = itemView.findViewById(R.id.text_created_at);
+            text_updatedAt = itemView.findViewById(R.id.text_updated_at);
+            text_category = itemView.findViewById(R.id.text_category);
+            text_joke = itemView.findViewById(R.id.text_joke);
+        }
+    }
+}
